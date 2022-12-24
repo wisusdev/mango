b09f35b49fb274f2826c82be6b7b31dfacf6cd5e
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-23 21:55:37 -0600

    Remove comments

--- app/core/Kernel.php
+++ app/core/Kernel.php
@@ -3,30 +3,17 @@ namespace App\core;
 
 class Kernel
 {
-	// Propiedades del framework
-	private $framework = 'molly';
-
-	// Funcion principal al instanciar la clase
 	function __construct() {
 		$this->init();
 	}
 
-	/**
-	 * Metodo para ejecutar otros metodos de forma subsecuente
-	 * @return void
-	 */
 	private function init() {
-		// Todos los metodos que se ejecutaran consecutivamente
 		Whoops::run();
 		$this->init_session();
 		new Csrf();
         new Routes();
 	}
 
-	/**
-	 * Metodo para iniciar la sesión en el sistema
-	 * @return void
-	 */
 	private function init_session(): void
     {
 		if (session_status() == PHP_SESSION_NONE){
@@ -34,9 +21,6 @@ class Kernel
 		}
 	}
 
-	/*
-	* Corre nuestro kernel
-	*/
 	public static function fly(): void
     {
 		new Kernel();
