0e08347552a68cf8ea7b8d088fef670e9c74264d
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-23 21:56:42 -0600

    Change directory

--- app/views/welcome.php
+++
@@ -1,6 +0,0 @@
-<?php include_once 'shared/header.php'; ?>
-	<?php include_once 'shared/navbar.php'; ?>
-
-	<h1>Creating an HTML 5 Document</h1>
-	<p>This is a body element</p>
-<?php include_once 'shared/footer.php'; ?>
