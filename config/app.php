350c31d907fd9cc433095997356e9df52c1d35fc
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-23 21:57:15 -0600

    app config

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'wisus',
+    'domain' => '127.0.0.1',
+    'port' => '8000',
+    'timezone' => date_default_timezone_set('America/El_Salvador'),
+    'facebook' => [
+        'key' => '123456789',
+        'value' => '987654321'
+    ]
+];
