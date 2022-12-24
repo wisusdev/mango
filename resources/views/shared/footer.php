19898d5232ee7112333edfb27764795a3555c04e
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-23 21:58:17 -0600

    shared files

---
+++ resources/views/shared/footer.php
@@ -0,0 +1,4 @@
+		<!-- JavaScript Bundle with Popper -->
+		<script src="<?php echo asset('js/bootstrap.bundle.min.js') ?>"></script>
+    </body>
+</html>
---
+++ resources/views/shared/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+    <html>
+    <head>
+        <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Basic Page Structure</title>
+		<!-- CSS only -->
+		<link rel="stylesheet" href="<?php echo asset('css/bootstrap.min.css') ?>">
+    </head>
+    <body>
