f70b915fcf49a846a595d510a8f85aa6dcc9efb1
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-23 21:58:36 -0600

    welcome view

---
+++ resources/views/welcome.php
@@ -0,0 +1,15 @@
+<?php include_once 'shared/header.php'; ?>
+	<?php include_once 'shared/navbar.php'; ?>
+
+	<div class="container">
+		<?php
+			echo config('app.facebook.key');
+		?>
+
+		<br>
+
+		<h1>Creating an HTML 5 Document</h1>
+		<p>This is a body element</p>
+	</div>
+
+<?php include_once 'shared/footer.php'; ?>
