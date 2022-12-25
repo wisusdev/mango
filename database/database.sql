74b96dc08895cee38a2c7bc2c27d5876520e1a6c
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-24 23:56:59 -0600

    Database

---
+++ database/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE molly;
+
+use molly;
+
+CREATE TABLE users (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
