58708d75349aae25cfaabe9d496f2c609dabe0ec
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-27 22:07:32 -0600

    app name and login url

--- resources/views/shared/navbar.php
+++ resources/views/shared/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-warning">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><?php echo config('app.name') ?></a>
+        <a class="navbar-brand" href="/"><?php echo config('app.name') ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -10,7 +10,7 @@
 			</ul>
             <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Login</a>
+                    <a class="nav-link" aria-current="page" href="<?php echo url('auth/login') ?>">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Register</a>
