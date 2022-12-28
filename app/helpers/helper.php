ae396ea911bf5b037b1fd0db2909312f58fee4d6
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-27 22:09:10 -0600

    New functions

--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\core\Csrf;
+
 function to_object($array) {
     return json_decode(json_encode($array));
 }
@@ -20,6 +23,26 @@ function get_domain(): string
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
     $domainName = $_SERVER['HTTP_HOST'].'/';
     return $protocol . $domainName;
+}
+
+function include_file(string $view, array $data = []): void
+{
+
+    $viewPath = './resources/views/' . $view . '.php';
+
+    // Convertir el array asociativo en un objeto
+    $d = to_object($data); // $data es un array y $d es un objeto
+
+    if (!is_file( $viewPath)){
+        die(sprintf('No existe la vista %s en el directorio', $view));
+    }
+
+    require_once $viewPath;
+}
+
+function url(string $route): string
+{
+    return get_domain() . $route;
 }
 
 function config(string $conf): string | array
@@ -50,4 +73,26 @@ function search_in_array(array $array, array $search, &$results){
             return $results;
         }
     }
+}
+
+function insert_inputs() {
+    $output = '';
+
+    if(isset($_POST['redirect_to'])){
+        $location = $_POST['redirect_to'];
+    } else if(isset($_GET['redirect_to'])){
+        $location = $_GET['redirect_to'];
+    } else {
+        $location = get_domain();
+    }
+
+    $csrf = new Csrf();
+    $csrf_token = $csrf->get_token();
+
+    $output .= '<input type="hidden" name="redirect_to" value="'.$location.'">';
+    $output .= '<input type="hidden" name="timecheck" value="'.time().'">';
+    $output .= '<input type="hidden" name="csrf" value="'.$csrf_token.'">';
+    $output .= '<input type="hidden" name="hook" value="jserp_hook">';
+    $output .= '<input type="hidden" name="action" value="post">';
+    return $output;
 }
