afe14965849e766cb1ee1ccbd83e644e295770b5
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-27 22:05:23 -0600

    Change database name and add options

--- config/database.php
+++ config/database.php
@@ -3,8 +3,11 @@
 return [
 	'DB_ENGINE' => 'mysql',
 	'DB_HOST' => 'localhost',
-	'DB_NAME' => 'beeframework',
+	'DB_NAME' => 'molly',
 	'DB_USER' => 'admin',
 	'DB_PASS' => '',
 	'DB_CHARSET' => 'utf8',
+    'options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ],
 ];
