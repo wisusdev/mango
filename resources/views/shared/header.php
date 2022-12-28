d1c63e363c3471360ed464a29aeb7c628ca3865b
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-12-27 22:07:01 -0600

    Update title

--- resources/views/shared/header.php
+++ resources/views/shared/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Basic Page Structure</title>
+		<title><?php echo config('app.name')?></title>
 		<!-- CSS only -->
 		<link rel="stylesheet" href="<?php echo asset('css/bootstrap.min.css') ?>">
     </head>
