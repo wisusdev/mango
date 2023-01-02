2dac8e881404a33cea9ac89899131bb6703c23a2
Jesus Avelar <wisusdev@gmail.com>
2023-01-01 19:10:44 -0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,82 @@
+// Webpack uses this to work with directories
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
 module.exports = {
-	entry: './resources/js/app.js',
-	output: {
-		path: path.resolve(__dirname, 'public/dist'),
-		filename: 'main.js'
+	// Path to your entry point. From this file Webpack will begin its work
+	entry: {
+		app: [
+			'./resources/js/app.js',
+			'./resources/scss/app.scss'
+		],
 	},
+
+	// Path and filename of your result bundle.
+	// Webpack will bundle all JavaScript into this file
+	output: {
+		path: path.resolve(__dirname, 'public/build'),
+		publicPath: '',
+		filename: 'bundle.js'
+	},
+
+	// Default mode for Webpack is production.
+	// Depending on mode Webpack will apply different things
+	// on the final bundle. For now, we don't need production's JavaScript
+	// minifying and other things, so let's set mode to development
+	mode: 'production',
+
 	module: {
 		rules: [
 			{
-				exclude: /node_modules/,
+				test: /\.js$/,
+				exclude: /(node_modules)/,
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-react'],
-						plugins: [
-							'@babel/plugin-proposal-optional-chaining',
-							'@babel/plugin-proposal-nullish-coalescing-operator'
-						]
+						presets: ['@babel/preset-env']
 					}
 				}
 			},
 			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				// Apply rule for .sass, .scss or .css files
+				test: /\.(sa|sc|c)ss$/,
+
+				// Set loaders to transform files.
+				// Loaders are applying from right to left(!)
+				// The first loader will be applied after others
+				use: [
+					{
+						// After all CSS loaders, we use a plugin to do its work.
+						// It gets all transformed CSS and extracts it into separate
+						// single bundled file
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						// This loader resolves url() and @imports inside CSS
+						loader: "css-loader",
+					},
+					{
+						// Then we apply postCSS fixes like autoprefixer and minifying
+						loader: "postcss-loader"
+					},
+					{
+						// First we transform SASS to standard CSS
+						loader: "sass-loader",
+						options: {
+							implementation: require("sass")
+						}
+					}
+				]
 			}
-		]
+		],
 	},
-	mode: 'development'
-}
+
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "bundle.css"
+		})
+	]
+};
