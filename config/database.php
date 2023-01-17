a79873a61bfed76fce18270f62c8ff9bb174e756
Jesus Avelar <wisusdev@gmail.com>
2023-01-17 06:56:57 -0600

    Improved database connection

--- config/database.php
+++ config/database.php
@@ -1,13 +1,10 @@
 <?php
 
 return [
-	'DB_ENGINE' => 'mysql',
-	'DB_HOST' => 'localhost',
-	'DB_NAME' => 'molly',
-	'DB_USER' => 'admin',
-	'DB_PASS' => '',
-	'DB_CHARSET' => 'utf8',
-    'options' => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ],
+	'db_engine' => 'mysql',
+	'db_host' => 'localhost',
+	'db_name' => 'mango',
+	'db_user' => 'root',
+	'db_pass' => '',
+	'db_charset' => 'utf8',
 ];
