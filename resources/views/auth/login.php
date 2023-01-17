12315fafff1e5cc89af4e1e3fe0fe82c4f634379
Jesus Avelar <wisusdev@gmail.com>
2023-01-17 06:53:52 -0600

    Design improvements

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -1,26 +1,34 @@
 <?php include_file('shared/header'); ?>
 <?php include_file('shared/navbar'); ?>
 
-<div class="container">
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo url('auth/post_login') ?>" method="post">
-			<?php echo insert_inputs(); ?>
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control">
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
+<div class="container pt-5">
+	<div class="row justify-content-center">
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-header">
+					<p class="m-0 fw-semibold">Login</p>
+				</div>
+				<div class="card-body">
+					<form action="<?php echo url('auth/post_login') ?>" method="post">
+                        <?php echo insert_inputs(); ?>
+						<div class="form-group mb-3">
+							<label class="form-label">Email</label>
+							<input type="email" name="email" class="form-control">
+						</div>
+						<div class="form-group mb-3">
+							<label class="form-label">Password</label>
+							<input type="password" name="password" class="form-control">
+						</div>
+						<div class="form-group float-end">
+							<input type="submit" class="btn btn-primary" value="Login">
+						</div>
 
-        </form>
-    </div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <?php include_file('shared/footer'); ?>
