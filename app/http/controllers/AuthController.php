6978529f22102a6b529d7d940d0f6fc6af2ec5d8
Jesus Avelar <wisusdev@gmail.com>
2023-01-17 07:07:49 -0600

    Auth controller

--- app/http/controllers/AuthController.php
+++ app/http/controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\http\controllers;
 
+use App\core\Redirect;
 use App\core\View;
+use App\models\User;
 
 class AuthController
 {
@@ -10,9 +12,26 @@ class AuthController
         View::render('auth/login');
     }
 
-    public function post_login(){
+    public function postLogin(){
         echo '<pre>';
-        print_r($_POST['username']);
+        print_r($_POST['email']);
         die();
+    }
+
+    public function register(){
+        View::render('auth/register');
+    }
+
+    public function postRegister(){
+        $data = [
+			'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'password' => $_POST['password']
+        ];
+
+        $user = new User();
+        $create = $user->create($data);
+
+		Redirect::to('auth/login');
     }
 }
